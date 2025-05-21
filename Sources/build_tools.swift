1c72b4afa6dfc440007db8b2dccaeb001f695b0c
Ryan Martin <eqpoqpe@gmail.com>
2025-05-21 13:50:13 +0800

    Add launch configuration and update build_tools with argument support

--- Sources/build_tools.swift
+++ Sources/build_tools.swift
@@ -1,6 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-// 
+//
 // Swift Argument Parser
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
 
@@ -8,6 +8,9 @@ import ArgumentParser
 
 @main
 struct build_tools: ParsableCommand {
+    @Argument(help: "The phrase to repeat.")
+    var phrase: String
+
     mutating func run() throws {
         print("Hello, world!")
     }
